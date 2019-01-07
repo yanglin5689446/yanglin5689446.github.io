2daa27565adf7b6977287a7dd76a091b6cdf7eba
Yang Lin <yanglin5689446@gmail.com>
2019-01-07 22:24:24 +0800

    Improve responsiveness

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,7 +1,6 @@
 
 import React from 'react'
 import ReactTooltip from 'react-tooltip'
-import Layout from 'layouts'
 import './style.scss'
 
 const SocialButtonLink = (props) => (
@@ -23,17 +22,17 @@ const Index = () => (
     <ReactTooltip effect='solid' />
     <div className='container px-3 py-5 p-md-5 index main-content'>
       <div>
-        <h2 className='title my-2 ml-3'>
+        <h2 className='title my-2 ml-3 h1'>
           About
         </h2>
-        <ul className='m-4 pb-3 chinese'>
+        <ul className='m-md-4 pb-3 chinese'>
           <li className="py-1">22 歲，血型 O 型，天蠍座。出生於台中，然而對自己住家半徑五公里內及一中街周邊外幾乎一無所知。</li>
           <li className="py-1">就讀於交大資工系大四，處於能畢業和不能畢業的疊加態。</li>
           <li className="py-1">會一點點電吉他，在交通大學星聲社擔任一介冗員。</li>
           <li className="py-1">浪費許多生命在滑 PTT、臉書以及 Reddit ，是不折不扣的鄉民。</li>
           <li className="py-1">最近想要多加強自己的設計能力，希望能不只當個切板仔，也能自己搞點好看又實用的產品。</li>
         </ul>
-        <h2 className='title my-2 ml-3'>
+        <h2 className='title my-2 ml-3 h1'>
           Interest
         </h2>
         <div className='m-4 pl-2 pb-3 chinese'>
@@ -44,7 +43,7 @@ const Index = () => (
           <p><i className='fas fa-tv mx-2'/>動畫、電影、日劇，美劇看得比較少</p>
           <p><i className='fa fa-music mx-2'/>基本上任何類型的音樂都聽，特愛老搖滾和金屬樂，會彈一點電吉他</p>
         </div>
-        <h2 className='title my-2 ml-3'>
+        <h2 className='title my-2 ml-3 h1'>
           Contacts
         </h2>
         <div className='m-4 pb-3'>
