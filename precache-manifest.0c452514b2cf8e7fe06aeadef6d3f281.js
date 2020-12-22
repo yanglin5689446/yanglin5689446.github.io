8454d7fe88a2b884dfb544a4a1edc6461d0ed278
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-12-22 11:44:19 +0000

    Deploy yanglin5689446/yanglin5689446.github.io to github.com/yanglin5689446/yanglin5689446.github.io.git:master

---
+++ precache-manifest.0c452514b2cf8e7fe06aeadef6d3f281.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f72010b2b651c892f51c75d00b7be5bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c890a309082cb7dd6a1f",
+    "url": "/static/css/main.5fed3d72.chunk.css"
+  },
+  {
+    "revision": "d8d21c72297284558dc4",
+    "url": "/static/js/2.195bb04c.chunk.js"
+  },
+  {
+    "revision": "c890a309082cb7dd6a1f",
+    "url": "/static/js/main.70fffcfc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1307a08582eb3ee44fb6b27be4211944",
+    "url": "/static/media/avatar.1307a085.jpg"
+  },
+  {
+    "revision": "bc2f3d3f9b6768da3afed41169c16df1",
+    "url": "/static/media/freeCell.bc2f3d3f.png"
+  },
+  {
+    "revision": "9403c559b6b864e49c6ac94be2b20827",
+    "url": "/static/media/linbox.9403c559.png"
+  }
+]);
